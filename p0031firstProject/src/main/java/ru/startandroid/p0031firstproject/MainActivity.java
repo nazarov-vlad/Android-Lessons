6510c9fcec88869d87d2f63185b62b6256e57964
nazarov-vlad <nazarowww@gmail.com>
2021-10-27 19:20:14 +0500

    27-10-2021_19-20-14

--- p0031firstProject/src/main/java/ru/startandroid/p0031firstproject/MainActivity.java
+++ p0031firstProject/src/main/java/ru/startandroid/p0031firstproject/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Комментарий для проверки синхронизации GitHUB
+        //Второй комментарий для синхронизации
     }
 }
